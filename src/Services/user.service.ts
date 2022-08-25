44a7a66f1d9d8ad969082e212ec7b7962a3ed7a8
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2022-08-25 09:28:57 -0300

    Criação de metodo de excluir de atualizar

--- src/Services/user.service.ts
+++ src/Services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { User } from 'src/Models/User';
 import { environment } from 'src/environments/environment';
-import { lastValueFrom } from 'rxjs';
+import { last, lastValueFrom } from 'rxjs';
 import { ErrorHttp } from 'src/Models/ErrorHttp';
 
 @Injectable({
@@ -12,7 +12,7 @@ export class UserService {
 
   constructor(private httpClient:HttpClient) { }
 
-  //Capturar todos os usuários
+  //[GET] Capturar todos os usuários
   public GetAllUser():Promise<string | User[]>{
     return lastValueFrom(this.httpClient.get<User[]>(`${environment.urlApi}`))
       .then(
@@ -27,13 +27,59 @@ export class UserService {
       );
   }
 
-  public GetByIdUser(id:number){
-    return lastValueFrom(this.httpClient.get<User>(`${environment.urlApi}`))
+  //[GET] Capturar usuário por ID
+  public GetByIdUser(id:number):Promise<string | User>{
+    return lastValueFrom(this.httpClient.get<User>(`${environment.urlApi}/1`))
       .then(
         (Response:User): User => {
           return Response;
         }
        )
+      .catch(
+        (Error:HttpErrorResponse):string => {
+          return ErrorHttp(Error);
+        }
+      );
+  }
+
+  //[POST] Criar usuário
+  public PostUser(user:User):Promise<string | User>{
+    return lastValueFrom(this.httpClient.post<User>(`${environment.urlApi}`,user))
+      .then(
+        (user:User):User => {
+          return user;
+        }
+      )
+      .catch(
+        (Error:HttpErrorResponse):string => {
+          return ErrorHttp(Error);
+        }
+      );
+  }
+
+  //[PUT] Atualizar usuário
+  public PutUser(id:number, user:User): Promise<string | User>{
+    return lastValueFrom(this.httpClient.put<User>(`${environment.urlApi}/${id}`,user))
+      .then(
+        (user:User):User => {
+          return user;
+        }
+      )
+      .catch(
+        (Error:HttpErrorResponse):string => {
+          return ErrorHttp(Error);
+        }
+      );
+  }
+
+  //[DELETE] Excluir usuário
+  public DeleteUser(id:number): Promise<string | null>{
+    return lastValueFrom(this.httpClient.delete<User>(`${environment.urlApi}/${id}`))
+      .then(
+        () => {
+          return null;
+        }
+      )
       .catch(
         (Error:HttpErrorResponse):string => {
           return ErrorHttp(Error);
